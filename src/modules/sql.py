b69cb18b3bea6a2041993d2b410fecda88ec260b
LOLNOA <hallow@autistici.org>
2025-02-15 14:25:21 -0500

    Revert sqli module changes

--- src/modules/sql.py
+++ src/modules/sql.py
@@ -1,14 +1,16 @@
+#SQLI FUNCTIONS MODUE\LE
 import threading
 import random
 import requests
 from datetime import datetime
-"""
-SQLI TESTING FUNCTION DEFS
-"""
 
 class Injthread(threading.Thread):
-    def __init__(self, hosts):
+    def __init__(self, hosts, vuln, col, vuln_scan_count, sqli_errors):
         self.hosts = hosts
+        self.vuln = vuln
+        self.col = col
+        self.vuln_scan_count = vuln_scan_count
+        self.sqli_errors = sqli_errors
         self.fcount = 0
         self.check = True
         threading.Thread.__init__(self)
@@ -18,7 +20,7 @@ class Injthread(threading.Thread):
         for url in urls:
             try:
                 if self.check:
-                    sqli_scanning(url)  # Call the function to scan
+                    sqli_scanning(url, self.vuln, self.col, self.vuln_scan_count, self.sqli_errors)
                 else:
                     break
             except KeyboardInterrupt:
@@ -29,13 +31,12 @@ class Injthread(threading.Thread):
         self.check = False
 
 def sqli_scanning(url, vuln, col, vuln_scan_count, sqli_errors):
-    # Removed print here to avoid printing all URLs during the scan
     vuln_scan_count.append(url)
     header = [line.strip() for line in open("lists/header", "r", encoding="utf-8")]
     ua = random.choice(header)
     headers = {"user-agent": ua}
     aug_url = url + "'"
-    
+
     try:
         r = requests.get(aug_url, timeout=2, headers=headers)
     except:
@@ -47,7 +48,7 @@ def sqli_scanning(url, vuln, col, vuln_scan_count, sqli_errors):
             try:
                 if str(error) in r.text and url not in remove_dups:
                     remove_dups.append(url)
-                    print(url + " is vulnerable --> %s" % str(error))
+                    print(f"{url} is vulnerable --> {str(error)}")
                     sqli_log_file.write("\n" + url)
                     vuln.append(url)
                     col.append(url)
@@ -55,20 +56,21 @@ def sqli_scanning(url, vuln, col, vuln_scan_count, sqli_errors):
             except:
                 pass
 
-def sqli_testing():
+def sqli_testing(usearch, numthreads, threads, vuln, col, vuln_scan_count, sqli_errors):
     global logfile
     global pulse
     global customlist
     global sql_list_counter
     global sql_list_count
     global sqli_confirmed
+
     pulse = datetime.now()
     vb = len(usearch) / int(numthreads)
     i = int(vb)
     m = len(usearch) % int(numthreads)
     z = 0
     print("[+] I'm working, please just hang out for a minute...\n")
-    
+
     try:
         if len(threads) <= int(numthreads):
             for x in range(0, int(numthreads)):
@@ -76,7 +78,7 @@ def sqli_testing():
                 if z < m:
                     sliced.append(usearch[int(numthreads) * i + z])
                     z += 1
-                thread = Injthread(sliced)
+                thread = Injthread(sliced, vuln, col, vuln_scan_count, sqli_errors)
                 thread.start()
                 threads.append(thread)
             for thread in threads:
